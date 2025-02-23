97837138724da2c45bc57ecf7b789e1a066dd8fc
ethux <me@ethux.nl>
2025-02-23 13:37:23 +0100

    Remove unnecessary imports for main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
 from src.api.Gitlab.router import router as gitlab_router
 
 app = FastAPI()
